7dbacc50f82321ae982762a01c973556a00ef9d0
1320071 <1700231@uad.ac.uk>
2021-07-22 22:01:11 +0100

    Added shuffle functionality

--- recipeBot.py
+++ recipeBot.py
@@ -6,23 +6,25 @@ from typing import List
 
 recipesList = []
 
-def ListRecipes():
+def GetRecipe():
     with open("recipes.csv") as recipes: 
         reader = csv.reader(recipes, delimiter=',')
         for row in reader:
             recipesList.append(row)
-        
-    print(f"There are {len(recipesList)} recipes")
-        
-    for r in recipesList:
-        print(r[0])
+    return(recipesList)
+    
+def ListRecipes(recipesList, all):
+    if all == True: 
+        print(f"There are {len(recipesList)} recipes")
+        for r in recipesList:
+            print(r[0])
+    else:
+        ShuffleRecipes(recipesList)
 
-def ShuffleRecipes():
-    recipe = ""
-    return recipe
+def ShuffleRecipes(recipesList):
+    del recipesList[0]
+    recipe = random.choice(recipesList)
+    print(recipe[0])
 
-def GetRecipe():
-    recipe = ""
-    return recipe
 
-ListRecipes()
+ListRecipes(GetRecipe(), False)
