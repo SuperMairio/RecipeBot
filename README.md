bb731e44605c11214d735dc07ba594e6666a3391
1320071 <1700231@uad.ac.uk>
2021-08-02 16:24:41 +0100

    Finished v 1.0

--- README.md
+++ README.md
@@ -2,8 +2,5 @@
 Easy way for me to store and find recipes
 
 ## TODO:
-- Add shuffle recipes functionality
-- Add get single recipe functionality
-- Add search functionality
 - (optional) Add discord functionality
 - (optional) Use google sheets instead of csv
--- recipeBot.py
+++ recipeBot.py
@@ -14,24 +14,19 @@ def GetRecipe():
         return(recipesList)
 
 def GetInput():
+    print("\n---------------------------------------\n")
     try:
-        choice = input("1 = Show all recipes\n2 = Get random recipe\n3 = Get specific recipe link\nq = Quit\nPlease choose one: ")
-        if choice == "q" or choice == "Q":
-            exit()
-        else:
-            return choice
+        choice = input("1 = Show all recipes\n2 = Get random recipe\n3 = Search recipes\nq = Quit\nPlease choose one: ")
+        return choice
     except(ValueError):
         print("Sorry I don't recognise that input")
         return GetInput()
 
-def ListRecipes(recipesList, x):
-    print("---------------------------------------")
-    if x == 1: 
-        print(f"There are {len(recipesList)} recipes")
-        for r in recipesList:
-            print(r[0])
-    elif x == 2:
-        ShuffleRecipes(recipesList)
+def ListRecipes(recipesList):
+    print(f"There are {len(recipesList)} recipes")
+    for r in recipesList:
+        print(r[0])
+
 
 def ShuffleRecipes(recipesList):
     del recipesList[0]
@@ -41,33 +36,43 @@ def ShuffleRecipes(recipesList):
 def SearchList(recipesList):
     x = 0
 
-    try:
-        choice = int(input("1 = Search recipes by letter\n2 = Search recipes by keyword\nPlease choose one: "))
-        if choice == 1:
-            letter = input("Please enter a letter: ")
-            for recipe in recipesList:
-                r = recipe[0]
-                if r[0] == letter:
-                    print(recipe[0])
-                    x += 1
-            if x == 0:
-                print("Sorry there are no recipes starting with that letter")
-                SearchList(recipesList)
-        elif choice == 2:
-            term = input("Please enter a keyword: ")
-            for recipe in recipesList:
-                if term in recipe[0]:
-                    print(recipe[0])
-                    x += 1
-            if x == 0:
-                print("Sorry there are no recipes with that word in them")
-                SearchList(recipesList)
-                
-    except(ValueError):
-        print("Sorry I don't recognise that input")
-        SearchList(recipesList)
+    choice = GetInput()
+    while choice.upper() != "Q":
+        if choice == "1": #List all
+            print("\n---------------------------------------\n")
+            ListRecipes(recipesList)
+            choice = GetInput()
 
-def RunBot():
-    x = GetInput()
-    
+        elif choice == "2": #Get random
+            print("\n---------------------------------------\n")
+            ShuffleRecipes(recipesList)
+            choice = GetInput()
+
+        elif choice == "3": #Search 
+            print("\n---------------------------------------\n")
+            searchType = int(input("1 = Search by letter\n2 = Search by keyword\nPlease choose one: "))
+            if searchType == 1:
+                letter = input("Please enter a letter: ")
+                print("\n")
+                for recipe in recipesList:
+                    r = recipe[0]
+                    if r[0] == letter:
+                        print(recipe[0])
+                        x += 1
+                if x == 0:
+                    print("Sorry there are no recipes starting with that letter")
+            elif searchType == 2:
+                term = input("Please enter a keyword: ")
+                print("\n")
+                for recipe in recipesList:
+                    if term in recipe[0]:
+                        print(recipe[0])
+                        x += 1
+                if x == 0:
+                    print("Sorry there are no recipes with that word in them")
+            choice = GetInput()
+    quit()
+
 print(" ---------- R E C I P E   B O T ----------")
+recipes = GetRecipe()
+SearchList(recipes)
